292b70c6cd11f17c45f0b5e59ab158cab6fa2c70
Nguyen Minh Luan <nguyenminhluan19051996@gmail.com>
2019-01-23 03:18:09 +0700

    Add files via upload

---
+++ spring-cloud-microservice/gateway-service/src/main/java/com/luandeptrai/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,18 @@
+package com.luandeptrai.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+}
+
