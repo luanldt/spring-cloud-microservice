309775d5d08b0b8467bf72c06ee6d012024891d2
Nguyen Minh Luan <nguyenminhluan19051996@gmail.com>
2019-01-24 00:33:47 +0700

    Add Admin Server
    Admin Server with email reminder

---
+++ spring-cloud-microservice/admin-server/src/main/java/com/luandeptrai/adminserver/AdminServerApplication.java
@@ -0,0 +1,21 @@
+package com.luandeptrai.adminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
+
